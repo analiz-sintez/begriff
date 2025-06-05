9cf49074a471780847d6c5ec0fc41f7c39815364
Kirill Krasnoshchekov <krvkir@gmail.com>
2025-06-05 23:42:29 +0400

    [add] `options` json field into `User` class (migration needed!)

--- app/core/models.py
+++ app/core/models.py
@@ -1,23 +1,69 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.types import JSON
+from sqlalchemy.ext.mutable import MutableDict
 from flask_sqlalchemy import SQLAlchemy
+import logging
 
-# It's tread-safe while it's from flask_sqlalchemy.
+# It's thread-safe while it's from flask_sqlalchemy.
 # If replacing flask with fastapi etc, refactor this
-# to make tread-safe.
+# to make thread-safe.
 db = SQLAlchemy()
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     login = Column(String, unique=True)
+    options = Column(MutableDict.as_mutable(JSON))
 
     def to_dict(self):
-        return {
-            'id': self.id,
-            'login': self.login
-        }
+        return {"id": self.id, "login": self.login}
 
     def __repr__(self):
-        return f"<User(login='{self.login}'>"
+        return f"<User(login='{self.login}')>"
+
+    def set_option(self, name, value):
+        if self.options is None:
+            self.options = {}
+        keys = name.split("/")
+        d = self.options
+        for key in keys[:-1]:
+            if key not in d or not isinstance(d[key], dict):
+                d[key] = {}
+            d = d[key]
+        d[keys[-1]] = value
+        logger.info(
+            "Setting option for user '%s': %s = %s", self.login, name, value
+        )
+        db.session.commit()
+
+    def get_option(self, name, default_value=None):
+        if not self.options:
+            logger.info(
+                "No options set for user '%s'. Returning default value for %s: %s",
+                self.login,
+                name,
+                default_value,
+            )
+            return default_value
+        keys = name.split("/")
+        d = self.options
+        for key in keys:
+            if key not in d:
+                logger.info(
+                    "Option '%s' not found for user '%s'. Returning default value: %s",
+                    name,
+                    self.login,
+                    default_value,
+                )
+                return default_value
+            d = d[key]
+        logger.info(
+            "Retrieved option for user '%s': %s = %s", self.login, name, d
+        )
+        return d
