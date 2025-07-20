ce2adb9d381677ced17fdd7b818467dc0274ba54
Kirill Krasnoshchekov <krvkir@gmail.com>
2025-07-20 06:45:28 +0400

    [add] a proper way to get initialized bus

--- core/bus/__init__.py
+++ core/bus/__init__.py
@@ -1,8 +1,11 @@
 from .service import Signal, Bus, encode, decode, make_regexp, unoption
 from .saving_backends import dump_signal_to_log, dump_signal_to_db
 
+bus = None
+
 
 def create_bus(config: object):
+    global bus
     if config.SIGNALS["logging_backend"] == "db":
         bus = Bus(saving_backend=dump_signal_to_db, config=config)
     elif config.SIGNALS["logging_backend"] == "log":
@@ -12,4 +15,8 @@ def create_bus(config: object):
             "Unknown config option for signals logging: %s"
             % config.SIGNALS["logging_backend"]
         )
+    return bus
+
+
+def get_bus():
     return bus
